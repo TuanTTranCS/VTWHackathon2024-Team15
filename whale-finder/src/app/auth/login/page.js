f12e6f6ad038a1e98bf5ea8efd34c2a8a78932b4
Tomas <44404123+tomeksen@users.noreply.github.com>
2024-09-14 13:28:54 -0700

    frontend deploy

---
+++ whale-finder/src/app/auth/login/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div>
+      <Button>Log in</Button>
+    </div>
+  );
+}
